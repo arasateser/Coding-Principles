4e35e1288425ea21308c67c93760a64957a6ac67
Aras Ateşer <43793683+arasateser@users.noreply.github.com>
2025-03-04 14:29:13 +0300

    DRY Principle price calculate example

---
+++ DRY/CarRentalService.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace codingPrinciples.DRY
+{
+    class CarRentalService
+    {
+        //DRY: Don't Repeat Yourself Principle
+        //avoid duplicating your code
+        //we are calculating price of multiple cars in our program
+        //but the formula is always the same
+        //so there is no need for different calculations for each car(it would have been a bad use od DRY)
+        //to avoid that we can create a single calculation formula and
+        //send details of each car whenever we need to
+
+        public decimal CalculatePrice(int days, decimal dailyRate)
+        {
+            decimal total = days * dailyRate;
+            if (days > 7) total *= 0.9m;
+            return total;
+        }
+    }
+}
---
+++ DRY/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace codingPrinciples.DRY
+{
+    class Program
+    {
+        static void Main()
+        {
+            CarRentalService rentalService = new CarRentalService();
+
+            decimal sedanPrice = rentalService.CalculatePrice(10, 50);
+            decimal goodCarPrice = rentalService.CalculatePrice(10, 40);
+
+            Console.WriteLine($"sedan price ${sedanPrice}");
+            Console.WriteLine($"Good car price ${goodCarPrice}");
+
+
+        }
+    }
+}
