e93536c9e20e0d5feca41e23df32813469e0c17c
Aras Ateşer <43793683+arasateser@users.noreply.github.com>
2025-03-04 15:03:14 +0300

    KISS principle food ordering example

---
+++ KISS/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace codingPrinciples.KISS
+{
+    class Program
+    {
+        public void Main()
+        {
+            foodOrder foodOrder = new foodOrder();
+
+            string size = Console.ReadLine()?.ToLower();
+
+            if (foodOrder.CalculatePrice(size) is decimal price)
+            {
+                Console.WriteLine($"food price is {price} for {size}");
+            }
+            else
+            {
+                Console.WriteLine("unavailable size");
+            }
+        }
+    }
+}
---
+++ KISS/foodOrder.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace codingPrinciples.KISS
+{
+    class foodOrder
+    {
+        //KISS: Keep It Stupid Simple
+        //our goal is the make our code as simple as possible
+        //in this example we assigned every size and their prices
+        //prices only change with their size and they are static values
+        //so we do not need different function for every size or
+        //switch cases we can simply create a dictionary for this specific purpose
+        private static readonly Dictionary<string, decimal> _sizePrices = new()
+        {
+            {"small",5 },
+            {"mid",10 },
+            {"big",12 }
+        };
+
+        public decimal CalculatePrice(string size)
+        {
+            return _sizePrices.TryGetValue(size.ToLower(), out decimal price)
+                ? price
+                : throw new ArgumentException("unavailable size");
+        }
+    }
+}
