9f72166aa4ca5d23c956d065261d7bebf0241afa
Aras Ateşer <43793683+arasateser@users.noreply.github.com>
2025-03-04 15:07:46 +0300

    YAGNI principle

---
+++ YAGNI/priceCalculate.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace codingPrinciples.YAGNI
+{
+
+    //YAGNI: You Ain't Gonna Need It
+    //do not add functionalities unless you reallly need it
+    //in example below we are adding values that we do not need for
+    //price calculation yet. that means we do not know if
+    //we will need it in the future or not
+    //as best practice its better to not include any not-used values in our code
+    public decimal priceCalculate(decimal price, int quantity, bool isLoyaltyMember, decimal discountPercentage = 0)
+    {
+        decimal total = price * quantity;
+
+        // Future feature: Apply discount for loyalty members
+        if (isLoyaltyMember)
+        {
+            total -= total * (discountPercentage / 100);
+        }
+
+        return total;
+    }
+}
