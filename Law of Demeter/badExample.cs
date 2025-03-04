6d44609cfa3f7916ec3f49332bf8bc5fc1fe40bc
Aras Ateşer <43793683+arasateser@users.noreply.github.com>
2025-03-04 15:20:48 +0300

    Law of Demeter coffee shop example

---
+++ Law of Demeter/badExample.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace codingPrinciples.Law_of_Demeter
+{
+    //Law of Demeter: Don't Talk to Strangers
+    //cashier in this shop has direct access inside of the coffe machine 
+    //which is not ideal
+    //cashier should only interact with barista not coffe machine
+    class CoffeeMachine
+    {
+        public void BrewCoffee() => Console.WriteLine("Brewing coffee...");
+    }
+
+    class Barista
+    {
+        public CoffeeMachine Machine { get; } = new CoffeeMachine();
+    }
+
+    class Cashier
+    {
+        private readonly Barista _barista;
+
+        public Cashier(Barista barista)
+        {
+            _barista = barista;
+        }
+
+        public void TakeOrder()
+        {
+            _barista.Machine.BrewCoffee();
+        }
+    }
+}
---
+++ Law of Demeter/goodExample.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace codingPrinciples.Law_of_Demeter
+{
+    //Law of Demeter: Don't Talk to Strangers
+    //cashier in this shop has direct access inside of the coffe machine 
+    //which is not ideal
+    //cashier should only interact with barista not coffe machine
+    class Barista
+    {
+        private readonly CoffeeMachine _machine = new CoffeeMachine();
+
+        public void MakeCoffee() => _machine.BrewCoffee();
+    }
+
+    class Cashier
+    {
+        private readonly Barista _barista;
+
+        public Cashier(Barista barista)
+        {
+            _barista = barista;
+        }
+
+        public void TakeOrder()
+        {
+            _barista.MakeCoffee();
+        }
+    }
+
+}
